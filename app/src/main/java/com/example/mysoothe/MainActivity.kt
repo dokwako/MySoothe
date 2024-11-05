4d28ab93dc2efe90bccc1db7022b811b7bb8cfc6
dokwako <denzilwawire418@gmail.com>
2024-11-05 12:09:54 +0300

    Initial commit

--- app/src/main/java/com/example/mysoothe/MainActivity.kt
+++ app/src/main/java/com/example/mysoothe/MainActivity.kt
@@ -16,32 +16,6 @@ import com.example.mysoothe.ui.theme.MySootheTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MySootheTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MySootheTheme {
-        Greeting("Android")
+        setContent { MySootheApp() }
     }
 }
