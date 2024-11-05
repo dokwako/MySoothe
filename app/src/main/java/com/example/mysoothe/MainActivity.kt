0fc59db4e68a390d7fabeadc045a823328823bb2
dokwako <denzilwawire418@gmail.com>
2024-11-05 23:27:01 +0300

    Initial commit

--- app/src/main/java/com/example/mysoothe/MainActivity.kt
+++ app/src/main/java/com/example/mysoothe/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,6 +23,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -62,14 +65,23 @@ fun SearchBar(
 }
 
 
-
-
 // Step: Align your body - Alignment
 @Composable
 fun AlignYourBodyElement(
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+    Column(
+        modifier = modifier
+    ){
+        Image(
+            painter = painterResource(R.drawable.ab1_inversions),
+            contentDescription = null,
+            modifier
+        )
+        Text(
+            text = stringResource(R.string.ab1_inversions)
+        )
+    }
 }
 
 // Step: Favorite collection card - Material Surface
